f3cfb8af64bd3e36613006b912ccb04192cc0132
lexter.tigas <lexter.tigas@silverlakeaxis.com>
2023-08-02 12:22:02 +0800

    use springfox-boot-starter instead of separated springfox-swagger dependencies

--- src/main/java/com/example/martrustbe/MarTrustBeApplication.java
+++ src/main/java/com/example/martrustbe/MarTrustBeApplication.java
@@ -3,10 +3,8 @@ package com.example.martrustbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableFeignClients
 public class MarTrustBeApplication {
 
